986559fb1442891b61a428e9cd8e9adeb86b8d4c
wuyixin <59789365+nengwuzhi@users.noreply.github.com>
2020-10-22 18:18:24 +0800

    Delete progress_bar.js

--- js/progress_bar.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myProgressBar").style.width = scrolled + "%";
-  document.getElementById("myProgressBar").innerHTML= Math.round(scrolled) + "%";
-}
